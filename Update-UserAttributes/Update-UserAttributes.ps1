945a9766434e1344c6ddafcca5922c1a0de00aca
Scott Pack <scott.pack@gmail.com>
2026-04-16 10:34:28 -0400

    Let's go ahead and wrap everything in a Main function.

--- Update-UserAttributes/Update-UserAttributes.ps1
+++ Update-UserAttributes/Update-UserAttributes.ps1
@@ -80,97 +80,103 @@ Function Connect-Modules
         Write-Error $_.Exception.message
         Exit
     }
-    Write-Information "Microsoft Graph PowerShell module is connected successfully" -ForegroundColor Cyan
+    Write-Verbose "Microsoft Graph PowerShell module is connected successfully"
 }
 
 Function Disconnect-Modules
 {
     Disconnect-MgGraph -ErrorAction SilentlyContinue|  Out-Null
-    Exit
 }
 
-
-$mgParams = @{}
-if ($PSBoundParameters.ContainsKey('TenantId'))             { $mgParams['TenantId']             = $TenantId }
-if ($PSBoundParameters.ContainsKey('ClientId'))             { $mgParams['ClientId']             = $ClientId}
-if ($PSBoundParameters.ContainsKey('CertificateThumbprint')){ $mgParams['CertificateThumbprint']= $CertificateThumbprint }
-if ($PSBoundParameters.ContainsKey('Environment'))          { $mgParams['Environment']          = $Environment}
-$mgParams['NoWelcome'] = $NoWelcome
-
-Connect-Modules -MgParams $mgParams
-
-# Since I'm using naive input detection let's define the Account list now
-$AccountList = @()
-
-if($CSVFilePath)
+Function Main
 {
-    $CSVFilePath = $CSVFilePath.Trim()
+    param([hashtable]$BoundParams)
 
-    # Import the CSV file into an array of objects if provided
-    try
+    $mgParams = @{}
+    if ($BoundParams.ContainsKey('TenantId'))             { $mgParams['TenantId']             = $TenantId }
+    if ($BoundParams.ContainsKey('ClientId'))             { $mgParams['ClientId']             = $ClientId}
+    if ($BoundParams.ContainsKey('CertificateThumbprint')){ $mgParams['CertificateThumbprint']= $CertificateThumbprint }
+    if ($BoundParams.ContainsKey('Environment'))          { $mgParams['Environment']          = $Environment}
+    $mgParams['NoWelcome'] = $NoWelcome
+
+    Connect-Modules -MgParams $mgParams
+
+    # Since I'm using naive input detection let's define the Account list now
+    $AccountList = @()
+
+    if($CSVFilePath)
     {
-        $csvImportData = Import-Csv -Path $CSVFilePath
+        $CSVFilePath = $CSVFilePath.Trim()
+
+        # Import the CSV file into an array of objects if provided
+        try
+        {
+            $csvImportData = Import-Csv -Path $CSVFilePath
+        }
+        catch
+        {
+            Write-Error $_.Exception.Message
+            Exit
+        }
+
+        # Run through the imported CSV data and builds the list of updates
+        foreach ($item in $csvImportData)
+        {
+            $tempHash = @{}
+            $item.psobject.properties | ForEach-Object { $tempHash[$_.Name.Trim("'")] = $_.Value.Trim("'") }
+            $AccountList += $tempHash
+        }
     }
-    catch
+    elseif($UserId)
     {
-        Write-Error $_.Exception.Message
-        Exit
+        # When working with only one user let's quick and dirty build the array as if we had a single entry CSV import. Only supports a single attribute update at a time but hey, it works and is easy to use for quick updates without needing to build a CSV file.
+        $AccountList += @{
+            'UserId'             = $UserId
+            $Attribute           = $Value
+        }
     }
 
-    # Run through the imported CSV data and builds the list of updates
-    foreach ($item in $csvImportData)
+
+    Foreach( $account in $AccountList)
     {
-        $tempHash = @{}
-        $item.psobject.properties | ForEach-Object { $tempHash[$_.Name.Trim("'")] = $_.Value.Trim("'") }
-        $AccountList += $tempHash
-    }
-}
-elseif($UserId)
-{
-    # When working with only one user let's quick and dirty build the array as if we had a single entry CSV import. Only supports a single attribute update at a time but hey, it works and is easy to use for quick updates without needing to build a CSV file.
-    $AccountList += @{
-        'UserId'             = $UserId
-        $Attribute           = $Value
-    }
-}
+        Write-Host "Processing $($account['UserId'])"
 
+        # Check to see if the manager is being updated since that requires a separate process
+        if($account['Manager'])
+        {
+            $ManagerId = (Get-MgUser -UserId $account['Manager']).Id
+            $NewManager = @{
+                "@odata.id"="https://graph.microsoft.com/v1.0/users/$($ManagerId)"
+            }
 
-Foreach( $account in $AccountList)
-{
-    Write-Information "Processing", $account['UserId']
+            try
+            {
+                Set-MgUserManagerByRef -UserId $account['UserId'] -BodyParameter $NewManager
+            }
+            catch
+            {
+                Write-Error $_.Exception.Message
+            }
 
-    # Check to see if the manager is being updated since that requires a separate process
-    if($account['Manager'])
-    {
-        $ManagerId = (Get-MgUser -UserId $account['Manager']).Id
-        $NewManager = @{
-            "@odata.id"="https://graph.microsoft.com/v1.0/users/$($ManagerId)"
+            #Remove the Manager field before continuing to process
+            $account.Remove('Manager')
         }
 
         try
         {
-            Set-MgUserManagerByRef -UserId $account['UserId'] -BodyParameter $NewManager
+            # Convert account psobject to a hashtable for splatting. Kind of gross, but is overall clean
+            $accountSplattable = $account | ConvertTo-Json | ConvertFrom-Json  -AsHashtable
+            Update-MgUser @accountSplattable
         }
         catch
         {
             Write-Error $_.Exception.Message
         }
 
-        #Remove the Manager field before continuing to process
-        $account.Remove('Manager')
     }
 
-    try
-    {
-        # Convert account psobject to a hashtable for splatting. Kind of gross, but is overall clean
-        $accountSplattable = $account | ConvertTo-Json | ConvertFrom-Json  -AsHashtable
-        Update-MgUser @accountSplattable
-    }
-    catch
-    {
-        Write-Error $_.Exception.Message
-    }
-
+    Disconnect-Modules
 }
 
-Disconnect-Modules
+
+Main -BoundParams $PSBoundParameters
